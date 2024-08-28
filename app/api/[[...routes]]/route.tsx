3a59b31c5a70f32dc8d87e65aaeb25922e062ba6
based-bren <alphabrendarious@gmail.com>
2024-08-28 10:52:06 +0100

    remove text

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -2,7 +2,6 @@
 
 import { Button, Frog, TextInput, parseEther } from 'frog'
 import { devtools } from 'frog/dev'
-//import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next'
 import { serveStatic } from 'frog/serve-static'
 import { baseSepolia } from 'viem/chains'
@@ -10,19 +9,15 @@ import { pinata } from 'frog/hubs'
 
 import { abi
 } from './abi.js'
-//import { Asap } from 'next/font/google/index.js'
+
 
 const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
-  // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
   hub: pinata(),
   title: 'Ham Pepe Test Mints',
 })
 
-// Uncomment to use Edge Runtime
-// export const runtime = 'edge'
 
 app.frame('/', (c) => {
 
@@ -31,7 +26,6 @@ app.frame('/', (c) => {
     image: `https://azure-ready-parakeet-471.mypinata.cloud/ipfs/QmW5n96dcHTrQkEBbsAA2qyRpRfFcsM6GVseZ8wy7v5t9Q`,
     imageAspectRatio: '1:1',
     intents: [
-      <TextInput placeholder=" type 1 up to 4 public mints" />,
       <Button.Transaction target="/freeMint">Free Mint</Button.Transaction>,
       <Button.Transaction target="/publicMint1">Mint 1</Button.Transaction>,
       <Button.Transaction target="/publicMint2">Mint 2</Button.Transaction>,
@@ -55,7 +49,6 @@ app.frame('/finish', (c) => {
 
 
 app.transaction('/freeMint', (c) => {
-  const { inputText } = c
   // Contract transaction response.
   return c.contract({
     abi,
@@ -67,7 +60,6 @@ app.transaction('/freeMint', (c) => {
 })
 
 app.transaction('/publicMint1', (c) => {
-  const { inputText } = c
   // Contract transaction response.
   return c.contract({
     abi,
@@ -80,7 +72,6 @@ app.transaction('/publicMint1', (c) => {
 })
 
 app.transaction('/publicMint2', (c) => {
-  const { inputText } = c
   // Contract transaction response.
   return c.contract({
     abi,
@@ -93,7 +84,6 @@ app.transaction('/publicMint2', (c) => {
 })
 
 app.transaction('/publicMint4', (c) => {
-  const { inputText } = c
   // Contract transaction response.
   return c.contract({
     abi,
@@ -105,33 +95,10 @@ app.transaction('/publicMint4', (c) => {
   })
 })
 
-app.transaction('/devMint', (c) => {
-  const { inputText } = c
-  // Contract transaction response.
-  return c.contract({
-    abi,
-    functionName: 'DevMint',
-    args: [inputText],
-    chainId: `eip155:${baseSepolia.id}`,
-    to: '0x18bbB7aF32317ea69CA7850A4602956534EBd10A',
-  })
-})
+
 
 devtools(app, { serveStatic })
 
 export const GET = handle(app)
 export const POST = handle(app)
 
-// NOTE: That if you are using the devtools and enable Edge Runtime, you will need to copy the devtools
-// static assets to the public folder. You can do this by adding a script to your package.json:
-// ```json
-// {
-//   scripts: {
-//     "copy-static": "cp -r ./node_modules/frog/_lib/ui/.frog ./public/.frog"
-//   }
-// }
-// ```
-// Next, you'll want to set up the devtools to use the correct assets path:
-// ```ts
-// devtools(app, { assetsPath: '/.frog' })
-// ```
