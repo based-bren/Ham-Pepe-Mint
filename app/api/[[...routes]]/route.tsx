fba385a4b7949b2e49e4dcad43467b963e86dcc1
based-bren <alphabrendarious@gmail.com>
2024-08-29 12:51:33 +0100

    added graphics

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -22,18 +22,46 @@ const app = new Frog({
 app.frame('/', (c) => {
 
   return c.res({
-    //action: '/finish',
-    image: `https://azure-ready-parakeet-471.mypinata.cloud/ipfs/QmW5n96dcHTrQkEBbsAA2qyRpRfFcsM6GVseZ8wy7v5t9Q`,
+    action: '/info',
+    image: `https://azure-ready-parakeet-471.mypinata.cloud/ipfs/QmXNj3y5SxcHu4MZYeTNEMrwG6aXRTJf8jF3FxDN1BGHRM`,
     imageAspectRatio: '1:1',
     intents: [
-      <Button.Transaction target="/freeMint">Free Mint</Button.Transaction>,
-      <Button.Transaction target="/publicMint1">Mint 1</Button.Transaction>,
-      <Button.Transaction target="/publicMint2">Mint 2</Button.Transaction>,
-      <Button.Transaction target="/publicMint4">Mint 4</Button.Transaction>,
-      //status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button value="start">Start</Button>,
     ],
   })
 })
+
+app.frame('/info', (c) => {
+
+  return c.res({
+  action: '/choose',
+  image: `https://azure-ready-parakeet-471.mypinata.cloud/ipfs/QmWuTWcSmK8MuMsMH6esZwuUYzCGEocetvB9cxyWqLZAHL`,
+  imageAspectRatio: '1:1',
+  intents: [
+    <Button value="info">Let's go!</Button>,
+    
+    //status === 'response' && <Button.Reset>Reset</Button.Reset>,
+  ],
+})
+})
+
+app.frame('/choose', (c) => {
+
+  return c.res({
+  //action: '/choose',
+  image: `https://azure-ready-parakeet-471.mypinata.cloud/ipfs/QmSyCq6U6MrfDwuvHSByVMcatViX85WbwobgbVvjMBGZbo`,
+  imageAspectRatio: '1:1',
+  intents: [
+    <Button.Transaction target="/freeMint">Free Mint</Button.Transaction>,
+    <Button.Transaction target="/publicMint1">Mint 1</Button.Transaction>,
+    <Button.Transaction target="/publicMint2">Mint 2</Button.Transaction>,
+    <Button.Transaction target="/publicMint4">Mint 4</Button.Transaction>,
+    //status === 'response' && <Button.Reset>Reset</Button.Reset>,
+  ],
+})
+})
+  
+
 
 app.frame('/finish', (c) => {
   const { transactionId } = c
